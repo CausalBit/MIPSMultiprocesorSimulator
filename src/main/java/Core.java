6807fec4721b99fbb972c7f939da55fb94e2f319
Irvin Umaña <irvin.umana@ucr.ac.cr>
2017-11-12 15:22:08 -0600

    added general quatum and synchornization rules

--- src/main/java/Core.java
+++ src/main/java/Core.java
@@ -8,7 +8,9 @@ import java.util.concurrent.Semaphore;
  */
 public class Core implements Runnable {
     private int clock;
+    private int quantum;
     private int pc;
+    private int numberOfCoresSystem;
     private boolean running;
     Semaphore barrier;
     HashMap<String, Integer> registers;
@@ -17,10 +19,12 @@ public class Core implements Runnable {
 
 
 
-    public Core(Semaphore barrier, int clock, int pc) {
+    public Core(Semaphore barrier, int clock, int pc, int quantum, int numberOfCoresSystem) {
         this.barrier = barrier;
         this.clock = clock;
         this.pc = pc;
+        this.quantum = quantum;
+        this.numberOfCoresSystem = numberOfCoresSystem;
 
         this.running = true;
 
@@ -35,11 +39,38 @@ public class Core implements Runnable {
     }
 
     public void run(){
+        int instructionDuration = 0;
+        boolean instructionIsFinished = false; //Cuando finaliza una instrucción, esta se modifica.
         while(running){
 
+            //Instrucción corre aquí.
 
+            //Cuando termine de correr una instrucción, o parte de esta, se incrementa el reloj.
+            clock++;
+            //La barrera va aquí para que sea necesario correr otro ciclo si ya no hay quantum.
+            if(barrier.getQueueLength() == numberOfCoresSystem - 1){
+                //es el último en llegar a la barrera, liberar todos.
+                barrier.release(numberOfCoresSystem-1);
+            }else{
+                try {
+                    barrier.acquire();
+                }catch( InterruptedException e ){
+                    e.printStackTrace();
+                }
+            }
+            //La verificación del cuantum está aquí.
+            if(instructionIsFinished && instructionDuration >= quantum){
+                //Store the current context if program is not finished (current instruction is not FIN).
+                //Take the next context from the queue.
+                //If the next context is null, then the core shuts down.
+                //The shut down implies decreasing the numberOfCoresSystem by one. :D
+            }
+            //TODO preguntarle a profe que si un hilo corre 40 ciclos en una instrucción (número de ejemplo)
+            //y otro hilo corre una instrucción por 20, y el otro por 10. Entonces los dos primeros
+            //hilos tiene que tiene que esperarse 20 y 30 respectivamente antes de seguir con la siguiente instrucción?
         }
     }
 
 
 }
+
