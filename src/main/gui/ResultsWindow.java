d7a5c60df1fb66502d58c007cadbf524f2be656e
JuanRodJ <juan.rodriguezjimenez@ucr.ac.cr>
2017-11-15 01:06:55 -0600

    Agregada ventana para solicitar datos al usuario. Cambios en ventana de resultados.

--- src/main/gui/ResultsWindow.java
+++ src/main/gui/ResultsWindow.java
@@ -49,6 +49,7 @@ public class ResultsWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public ResultsWindow() {
+		setTitle("Results");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1057, 795);
